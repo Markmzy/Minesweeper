ebf3fdef8e3519955225cc381f154613647e19ed
Zhiyue Ma <zhiyuem1@uci.edu>
2020-04-27 19:55:43 -0700

    CLion files

---
+++ Minesweeper_Cpp/cmake-build-debug/CMakeFiles/Minesweeper_Cpp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/src/Main.cpp" "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/cmake-build-debug/CMakeFiles/Minesweeper_Cpp.dir/src/Main.cpp.o"
+  "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/src/MyAI.cpp" "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/cmake-build-debug/CMakeFiles/Minesweeper_Cpp.dir/src/MyAI.cpp.o"
+  "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/src/World.cpp" "/Users/Markmzy/Desktop/Minesweeper/Minesweeper_Cpp/cmake-build-debug/CMakeFiles/Minesweeper_Cpp.dir/src/World.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Minesweeper_Cpp/cmake-build-debug/CMakeFiles/Minesweeper_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Minesweeper_Cpp.dir/src/Main.cpp.o"
+  "CMakeFiles/Minesweeper_Cpp.dir/src/MyAI.cpp.o"
+  "CMakeFiles/Minesweeper_Cpp.dir/src/World.cpp.o"
+  "Minesweeper_Cpp"
+  "Minesweeper_Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Minesweeper_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
